08676e8cbbead684b095500ed08edd4b1e305062
Damion Junk <junkda@gmail.com>
2012-10-25 16:38:56 -0400

    version cleanup

--- project.clj
+++ project.clj
@@ -8,11 +8,12 @@
                  [org.clojure/tools.logging "0.2.3"]
                  [org.slf4j/slf4j-log4j12 "1.6.4"]
                  [cheshire "4.0.2"]
-                 [sentimental "0.1.1-SNAPSHOT"]
+                 [sentimental "0.1.1"]
                  [sentimental-data "0.1.0"]
-                 [wujuko-common "0.1.1-SNAPSHOT"]
+                 [wujuko-common "0.1.1"]
                  [org.zeromq/jzmq "1.1.0-SNAPSHOT"]]
   :aliases {"zmqanew" ["trampoline" "run" "zmq-anew3l.core"]}
   :main zmq-anew3l.core
   ;; Note this must match on all systems installed...
-  :jvm-opts ["-Djava.library.path=/usr/local/lib/" "-Xmx1024m" "-server" "-XX:MaxPermSize=256m"])
+  :jvm-opts ["-Djava.library.path=/usr/local/lib/" "-Xmx1024m" "-server"
+             "-XX:MaxPermSize=256m"])
